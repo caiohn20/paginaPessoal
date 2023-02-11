2f8cb103bbbdde138f72b2a09bda20e0c9e45d14
caiohn20 <caio.hn20@gmail.com>
2023-02-11 18:24:49 -0300

    estilizando página

--- src/Views/home.php
+++ src/Views/home.php
@@ -4,37 +4,50 @@ require_once './Layout/cabecalho.php';
 ?>
 
 <main>
-
     <section class="text-center container m-0">
         <div class="row">
-            <div class="col-6 backgroundAzul">
-                <div class="row mt-5">
-                    <div class="col-12">
-                        <p class="letraAmarela text-start fs-2">Olá, Sou</p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <p class="text-start fs-1">Caio Henrique</p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <p class="text-start fs-1">Natal</p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12">
-                        <p class="text-start">Empreendedor / Desenvolvedor</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-6 p-0">
+            <div class="col-md-6 col-sm-12 backgroundDivHome">
 
+                <div class="row mt-md-5 mx-md-5 px-4">
+                    <div class="col-12 p-0">
+                        <p class="letraAmarela text-start fs-2 fw-bold">Olá, Sou</p>
+                    </div>
+                </div>
+                <div class="row mx-md-5 px-4">
+                    <div class="col-12 p-0">
+                        <p class="text-start fs-1 fw-semibold textoClaro">Caio Henrique</p>
+                    </div>
+                </div>
+                <div class="row mx-md-5 px-4">
+                    <div class="col-12 p-0">
+                        <p class="text-start fs-1 fw-semibold textoClaro">Natal</p>
+                    </div>
+                </div>
+                <div class="row mx-md-5 px-4">
+                    <div class="col-12 p-0">
+                        <p class="text-start fw-normal textoClaro">Empreendedor / Desenvolvedor</p>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="col-md-6 col-sm-12 p-0" id="divImagemUm">
             </div>
         </div>
     </section>
-
+    <section class="text-center container my-5">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 mt-md-4">
+                <h1 class="fw-bold">Sobre Mim</h1>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-8 col-sm-12 mt-md-4 text-center">
+                <p>Sou um parágrafo. Clique para editar e adicionar o seu texto. É fácil! Basta clicar em "Editar Texto" ou clicar duas vezes sobre mim e você poderá adicionar o seu próprio conteúdo e trocar fontes. Sinta-se à vontade para arrastar e soltar em qualquer lugar da página. Sou um ótimo espaço para contar um pouco mais sobre você.</p>
+                <p>Sou um parágrafo. Clique para editar e adicionar o seu texto. É fácil! Basta clicar em "Editar Texto" ou clicar duas vezes sobre mim e você poderá adicionar o seu próprio conteúdo e trocar fontes. Sinta-se à vontade para arrastar e soltar em qualquer lugar da página. Sou um ótimo espaço para contar um pouco mais sobre você.</p>
+            </div>
+        </div>
+    </section>
 </main>
 
 <?php
