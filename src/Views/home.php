9a78a91c8aa6d4418e27b966a45c944b2f239a18
caiohn20 <caio.hn20@gmail.com>
2023-02-12 19:43:33 -0300

    sessão contato

--- src/Views/home.php
+++ src/Views/home.php
@@ -51,27 +51,113 @@ require_once './Layout/cabecalho.php';
         </div>
         <div class="row justify-content-center mb-5">
             <div class="col-md-8 col-sm-12 mt-md-4 text-center mb-5">
-                <a  class="btn btn-warning" href="https://www.linkedin.com/in/caiohn20/" target="_blank">Ver currículo completo</a>
+                <a class="btn btn-warning" href="https://www.linkedin.com/in/caiohn20/" target="_blank">Ver currículo completo</a>
             </div>
         </div>
     </section>
     <section class="text-center container py-5">
-        <div class="row">
+        <div class="row mt-5">
             <div class="col-md-12 col-sm-12 my-md-5">
-                <h1 class="fw-bold tituloSessao mb-2">Fotos</h1>
+                <h1 class="fw-bold tituloSessao mb-2">Clientes e Projetos</h1>
             </div>
         </div>
         <div class="row justify-content-center">
-            <div class="col-md-6 col-sm-12 mt-md-4 text-center p-0" id="divImagemDois">
-                <img src="../assets/imgs/foto2.jpg" alt="Foto1">
+            <div class="col-md-4 col-sm-12 text-center">
+                <div class="card" style="border: 0;">
+                    <div class="card-body text-start ps-0">
+                        <div class="row">
+                            <div class="col-12">
+                                <p class="m-0 fw-semibold bordaEsquerdaAzul"><span class="ms-4 fs-4">Grupo Maxpro Educacional</span></p>
+                                <p class="m-0 bordaEsquerdaAzul"><span class="ms-4">ProCorp - Gestão de Redes</span></p>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-12">
+                                <p class="ms-4">Sistema é usado para gerenciar a parte financeira, educacional e de cadastros dos alunos da rede Micropro. Utilização de PHP Orientado a Objeto, bem como o emprego dos padrões Factory e DAO. Emprego da arquitetura MVC nos sistemas desenvolvidos. Utilização de Jquery e AJAX. Utilização da linguagem de estilos CSS, com o auxílio do framework bootstrap. Manutenção e manipulação de banco de dados MySQL em Linux. Emissão de notas ficais nas matrículas e dos produtos vendidos pela empresa através de consultas via API web services REST.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-6 col-sm-12 mt-md-4 text-center p-0" id="divImagemTres">
-                <img src="../assets/imgs/foto3.jpg" alt="Foto2">
+            <div class="col-md-4 col-sm-12 text-center">
+                <div class="card" style="border: 0;">
+                    <div class="card-body text-start ps-0">
+                        <div class="row">
+                            <div class="col-12">
+                                <p class="m-0 fw-semibold bordaEsquerdaAzul"><span class="ms-4 fs-4">FSID - Portal Cliente</span></p>
+                                <p class="m-0 bordaEsquerdaAzul"><span class="ms-4">Foursys / Volkswagen Brasil</span></p>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-12">
+                                <p class="ms-4">O FSID é uma autenticação universal de dois fatores que permite ao cliente o acesso a todos os sistemas da Volkswagen Brasil que possuam essa inteligência. Utilizamos o pacote keycloak para a autenticação e garantia de segurança dos sistemas.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-12 text-center">
+                <div class="card" style="border: 0;">
+                    <div class="card-body text-start">
+                        <div class="row">
+                            <div class="col-12">
+                                <p class="m-0 fw-semibold bordaEsquerdaAzul"><span class="ms-4 fs-4">QrCode</span></p>
+                                <p class="m-0 bordaEsquerdaAzul"><span class="ms-4">Foursys / Volkswagen Brasil</span></p>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-12">
+                                <p class="ms-4">API capaz de gerar QR-Code a partir de uma string e entregar uma imagem que represente esse QR-Code para aplicação onde for necessário.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
-    <section class="text-center container">
+    <section class="text-center container py-5">
         <div class="row justify-content-center">
+            <div class="col-md-6 col-sm-12 p-0" id="divImagemUm">
+            </div>
+            <div class="col-md-6 col-sm-12 backgroundDivHome">
+
+                <div style="height: auto;">
+
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="text-start fw-semibold letraAmarela tituloSessao">Contato</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="text-start fw-semibold fs-5">Caio Henrique Natal</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="text-start fw-semibold fs-5">Email - caio.hn20@gmail.com</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="text-start fw-semibold fs-5">Tel - (19) 997877712</p>
+                        </div>
+                    </div>
+                    <div class="row justify-content-center mt-4">
+                        <div class="col">
+                            <a href="https://instagram.com/caiohn20?igshid=YzgyMTM2MGM=" target="_blank" class="btn"><i class="fa-brands fa-instagram" style="scale: 3;"></i></a>
+                        </div>
+                        <div class="col">
+                            <a href="https://www.facebook.com/caio.hn20?mibextid=ZbWKwL" target="_blank" class="btn"><i class="fa-brands fa-facebook" style="scale: 3;"></i></a>
+                        </div>
+                        <div class="col">
+                            <a href="https://github.com/caiohn20" target="_blank" class="btn"><i class="fa-brands fa-github" style="scale: 3;"></i></a>
+                        </div>
+                    </div>
+                    
+                </div>
+
+            </div>
         </div>
     </section>
 </main>
