d3204af063b92a5b6e1678d5863a963ee23262af
caiohn20 <caio.hn20@gmail.com>
2023-02-12 12:02:16 -0300

    sessão sobre mim e correção responsividade

--- src/Views/home.php
+++ src/Views/home.php
@@ -8,24 +8,26 @@ require_once './Layout/cabecalho.php';
         <div class="row">
             <div class="col-md-6 col-sm-12 backgroundDivHome">
 
-                <div class="row mt-md-5 mx-md-5 px-4">
-                    <div class="col-12 p-0">
-                        <p class="letraAmarela text-start fs-2 fw-bold">Olá, Sou</p>
+                <div style="height: auto;">
+                    <div class="row mx-md-5 px-md-4">
+                        <div class="col-12 p-0">
+                            <p class="letraAmarela fw-semibold text-start subtitulo">Olá, Sou</p>
+                        </div>
                     </div>
-                </div>
-                <div class="row mx-md-5 px-4">
-                    <div class="col-12 p-0">
-                        <p class="text-start fs-1 fw-semibold textoClaro">Caio Henrique</p>
+                    <div class="row mx-md-5 px-md-4">
+                        <div class="col-12 p-0">
+                            <p class="text-start fw-semibold textoClaro titulo">Caio Henrique</p>
+                        </div>
                     </div>
-                </div>
-                <div class="row mx-md-5 px-4">
-                    <div class="col-12 p-0">
-                        <p class="text-start fs-1 fw-semibold textoClaro">Natal</p>
+                    <div class="row mx-md-5 px-md-4">
+                        <div class="col-12 p-0">
+                            <p class="text-start fw-semibold textoClaro titulo">Natal</p>
+                        </div>
                     </div>
-                </div>
-                <div class="row mx-md-5 px-4">
-                    <div class="col-12 p-0">
-                        <p class="text-start fw-normal textoClaro">Empreendedor / Desenvolvedor</p>
+                    <div class="row mx-md-5 px-md-4">
+                        <div class="col-12 p-0">
+                            <p class="text-start fs-4 fw-normal textoClaro">Empreendedor / Desenvolvedor</p>
+                        </div>
                     </div>
                 </div>
 
@@ -35,16 +37,66 @@ require_once './Layout/cabecalho.php';
             </div>
         </div>
     </section>
-    <section class="text-center container my-5">
-        <div class="row">
+    <section class="text-center container py-5" style="background-color: rgb(241, 240, 240);">
+        <div class="row mt-5">
             <div class="col-md-12 col-sm-12 mt-md-4">
                 <h1 class="fw-bold">Sobre Mim</h1>
             </div>
         </div>
-        <div class="row justify-content-center">
+        <div class="row justify-content-center mb-5">
             <div class="col-md-8 col-sm-12 mt-md-4 text-center">
                 <p>Sou um parágrafo. Clique para editar e adicionar o seu texto. É fácil! Basta clicar em "Editar Texto" ou clicar duas vezes sobre mim e você poderá adicionar o seu próprio conteúdo e trocar fontes. Sinta-se à vontade para arrastar e soltar em qualquer lugar da página. Sou um ótimo espaço para contar um pouco mais sobre você.</p>
                 <p>Sou um parágrafo. Clique para editar e adicionar o seu texto. É fácil! Basta clicar em "Editar Texto" ou clicar duas vezes sobre mim e você poderá adicionar o seu próprio conteúdo e trocar fontes. Sinta-se à vontade para arrastar e soltar em qualquer lugar da página. Sou um ótimo espaço para contar um pouco mais sobre você.</p>
+            </div>
+        </div>
+        <div class="row justify-content-center mb-5">
+            <div class="col-md-8 col-sm-12 mt-md-4 text-center">
+                <a  class="btn btn-warning" href="https://www.linkedin.com/in/caiohn20/" target="_blank">Ver currículo completo</a>
+            </div>
+        </div>
+    </section>
+    <section class="text-center container py-5">
+        <div class="row">
+            <div class="col-md-12 col-sm-12 mt-md-4">
+                <h1 class="fw-bold">Fotos</h1>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-sm-12 mt-md-4 text-center p-0" id="divImagemDois">
+                <img src="../assets/imgs/foto2.jpg" alt="Foto1">
+            </div>
+            <div class="col-md-6 col-sm-12 mt-md-4 text-center p-0" id="divImagemTres">
+                <img src="../assets/imgs/foto3.jpg" alt="Foto2">
+            </div>
+        </div>
+    </section>
+    <section class="text-center container">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-sm-12 backgroundDivHome">
+
+                <div style="height: auto;">
+                    <div class="row mx-md-5 px-md-4">
+                        <div class="col-12 p-0">
+                            <p class="letraAmarela fw-semibold text-start subtitulo">Olá, Sou</p>
+                        </div>
+                    </div>
+                    <div class="row mx-md-5 px-md-4">
+                        <div class="col-12 p-0">
+                            <p class="text-start fw-semibold textoClaro titulo">Caio Henrique</p>
+                        </div>
+                    </div>
+                    <div class="row mx-md-5 px-md-4">
+                        <div class="col-12 p-0">
+                            <p class="text-start fw-semibold textoClaro titulo">Natal</p>
+                        </div>
+                    </div>
+                    <div class="row mx-md-5 px-md-4">
+                        <div class="col-12 p-0">
+                            <p class="text-start fs-4 fw-normal textoClaro">Empreendedor / Desenvolvedor</p>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </section>
